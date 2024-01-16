18691f6bcba645cfe71d69102cf7f001b896c93c
Seiry Yu <seiry@squareup.com>
2024-01-16 16:41:59 +0800

    Remove unused CSS and update page component

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
     experimental: {
         swcPlugins: [
-            ['@swc-jotai/react-refresh', {}],
+            // ['@swc-jotai/react-refresh', {}],
         ],
     },
 }
